b73817a9785250892b82c2165f2ca28ee6a78be8
Hilton santos <jhilton930@gmail.com>
2026-08-13 20:16:58 -0300

    feat: adiciona entidades e repositories dos cadastros realizados

---
+++ backend/src/main/kotlin/br/com/jhiltonsantos/matricula/domain/Curso.kt
@@ -0,0 +1,16 @@
+package br.com.jhiltonsantos.matricula.domain
+
+import io.quarkus.hibernate.orm.panache.kotlin.PanacheEntityBase
+import jakarta.persistence.Entity
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+import java.util.UUID
+
+@Entity
+@Table(name = "curso")
+class Curso(
+    var nome: String,
+) : PanacheEntityBase {
+    @Id
+    var id: UUID? = null
+}
