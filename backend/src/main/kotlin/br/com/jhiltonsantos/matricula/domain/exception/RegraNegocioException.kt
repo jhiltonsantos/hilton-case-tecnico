a56daf7630120a0a9da150eeeb7fa0b2c23fda8f
Hilton santos <jhilton930@gmail.com>
2026-08-14 10:13:50 -0300

    feat: adiciona excecoes de regra da matricula

--- backend/src/main/kotlin/br/com/jhiltonsantos/matricula/domain/exception/RegraNegocioException.kt
+++ backend/src/main/kotlin/br/com/jhiltonsantos/matricula/domain/exception/RegraNegocioException.kt
@@ -6,3 +6,12 @@ open class RegraNegocioException(mensagem: String) : RuntimeException(mensagem);
 
 class AulaComMatriculadosException(id: UUID) :
     RegraNegocioException(mensagem = "Aula possui alunos matriculados, exclusao nao permitida: $id")
+
+class CursoNaoAutorizadoException(aulaId: UUID) :
+    RegraNegocioException(mensagem = "Curso do aluno nao autorizado para a aula: $aulaId")
+
+class ChoqueDeHorarioException(aulaId: UUID) :
+    RegraNegocioException(mensagem = "Aula conflita com horario de outra matricula ativa: $aulaId")
+
+class VagaIndisponivelException(aulaId: UUID) :
+    RegraNegocioException(mensagem = "Nao ha vaga disponivel na aula: $aulaId")
