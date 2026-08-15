3c8afcc179a178ae9dcdf2bf80d7890b02baf057
Hilton santos <jhilton930@gmail.com>
2026-08-14 21:51:06 -0300

    feat: customiza tema do primeng

--- frontend/apps/matricula/src/app/app.config.ts
+++ frontend/apps/matricula/src/app/app.config.ts
@@ -6,7 +6,26 @@ import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
+import { definePreset } from '@primeuix/themes';
 import Aura from '@primeuix/themes/aura';
+
+const MatriculaPreset = definePreset(Aura, {
+  semantic: {
+    primary: {
+      50: '{amber.50}',
+      100: '{amber.100}',
+      200: '{amber.200}',
+      300: '{amber.300}',
+      400: '{amber.400}',
+      500: '{amber.500}',
+      600: '{amber.600}',
+      700: '{amber.700}',
+      800: '{amber.800}',
+      900: '{amber.900}',
+      950: '{amber.950}',
+    },
+  },
+});
 import {
   provideKeycloak,
   createInterceptorCondition,
@@ -30,7 +49,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideAnimationsAsync(),
-    providePrimeNG({ theme: { preset: Aura } }),
+    providePrimeNG({ theme: { preset: MatriculaPreset } }),
     provideKeycloak({
       config: {
         url: 'http://localhost:8081',
